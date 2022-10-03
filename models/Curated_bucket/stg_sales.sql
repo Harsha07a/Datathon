cb83f72b9f68934567b9a72df1bbc6d2306e71a3
hatri@deloitte.com.au <hatri@deloitte.com.au>
2022-10-03 02:44:49 +0000

    v2_stg_tables

--- models/Curated_bucket/stg_sales.sql
+++ models/Curated_bucket/stg_sales.sql
@@ -1,6 +1,18 @@
 select
-RECEIPT_ID
+DISTINCT RECEIPT_ID,
 STORE_ID,
 product_barcode,
 date_date
 FROM {{source ('raw_fmcg_data','RAW_MP_ID_FMCG')}}
+
+
+/*select
+COUNT (DISTINCT RECEIPT_ID),
+RECEIPT_ID,
+STORE_ID,
+DATE_DATE,
+PRODUCT_BARCODE
+FROM {{source ('raw_fmcg_data','RAW_MP_ID_FMCG')}}
+GROUP BY RECEIPT_ID,DATE_DATE,
+PRODUCT_BARCODE,STORE_ID*/
+
